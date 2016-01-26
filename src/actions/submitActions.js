1f983eda7e0b2b8ccf489e1c870a1355a6762f99
dav <dg222cs@student.lnu.se>
2016-01-26 14:00:23 +0100

    ADD: Storing in firebase is done.

--- src/actions/submitActions.js
+++ src/actions/submitActions.js
@@ -2,21 +2,25 @@
  * Created by dav on 2016-01-13.
  */
 import base64Encode from './../utils/base64Encode';
+import Firebase from 'firebase';
+
+const submitToFirebase = (portfolioCard) => {
+    let fireBaseRef = new Firebase('portfoliodavidg.firebaseIO.com/' + 'portfolio/');
+    fireBaseRef.push(portfolioCard);
+};
+
 export default {
     submit(portfolioCard) {
         return async (dispatch) => {
             dispatch({type: 'START_SPINNER'});
             base64Encode(portfolioCard.image)
-                .then((result) => console.log(result))
+                .then((result) => {
+                    portfolioCard.image = result;
+                    submitToFirebase(portfolioCard)
+                })
                 .catch((err) => {
                    console.log('error', err);
                 });
-            let submit = await notImplementedFunction(portfolioCard);
-            if (submit.isSuccessful) {
-                dispatch({type: 'SUBMIT_PORTFOLIO_CARD'});
-            } else {
-                console.log("Something went wrong.")
-            }
         }
     }
 }
